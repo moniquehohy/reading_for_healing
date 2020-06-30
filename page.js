f8f08859dd3f0c54b8c74870702579211e42d6bf
GMKing18 <65235988+GMKing18@users.noreply.github.com>
2020-06-30 18:05:04 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,10 @@
+function promptMe(){
+    var likeReview= prompt ("Welcome, did you like this review?");
+    if(likeReview.toLowerCase === "yes"){
+        alert ('yay, thank you for reading!');
+    }
+    else {
+        alert("Thank you for reading");
+    };
+
+}
